6a13783fd25eec9e9d7a7045a34d105f253cbb99
HustonMavr <huston.mavr@gmail.com>
2018-04-10 11:21:10 +0300

    added client app

---
+++ client-app/src/sample/Controller.java
@@ -0,0 +1,74 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.File;
+
+public class Controller extends Application {
+    private Scene scene;
+    @Override public void start(Stage stage) {
+        // create the scene
+        stage.setTitle("Web View");
+        scene = new Scene(new Browser(),750,500, Color.web("#666970"));
+        stage.setScene(scene);
+        scene.getStylesheets().add("webviewsample/BrowserToolbar.css");
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
+class Browser extends Region {
+
+    final WebView browser = new WebView();
+    final WebEngine webEngine = browser.getEngine();
+
+    public Browser() {
+        //apply the styles
+        getStyleClass().add("browser");
+        // load the web page
+        String htmlData;
+
+        File f = new File("C:/Users/mavr/Desktop/test-proto/index.html");
+       //URL url = this.getClass().getResource("C:/Users/mavr/Desktop/test-proto/index.html");
+        webEngine.load("http://color-it.herokuapp.com");//"file:///C:/Users/mavr/Desktop/test-proto/index.html");
+        getChildren().add(browser);
+
+    }
+    private Node createSpacer() {
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        return spacer;
+    }
+
+    @Override protected void layoutChildren() {
+        double w = getWidth();
+        double h = getHeight();
+        layoutInArea(browser,0,0,w,h,0, HPos.CENTER, VPos.CENTER);
+    }
+
+    @Override protected double computePrefWidth(double height) {
+        return 750;
+    }
+
+    @Override protected double computePrefHeight(double width) {
+        return 500;
+    }
+}
