6c20e5f4787bece10b13156c49b267ef76d05433
HustonMmmavr <huston.mavr@gmail.com>
2018-05-20 17:50:48 +0300

    client refactor

--- services/client/src/main/java/Controller.java
+++ services/client/src/main/java/Controller.java
@@ -1,22 +1,7 @@
 import javafx.application.Application;
-import javafx.geometry.HPos;
-import javafx.geometry.VPos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
 import javafx.stage.Stage;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.io.File;
 
 public class Controller extends Application {
     private Scene scene;
@@ -33,43 +18,5 @@ public class Controller extends Application {
 
     public static void main(String[] args) {
         launch(args);
-    }
-}
-
-class Browser extends Region {
-
-    final WebView browser = new WebView();
-    final WebEngine webEngine = browser.getEngine();
-
-    public Browser() {
-        //apply the styles
-        getStyleClass().add("browser");
-        // load the web page
-        webEngine.load("file:///home/mavr/test-proto/index.html");//"http://color-it.herokuapp.com");//"file:///C:/Users/mavr/Desktop/test-proto/index.html");
-        getChildren().add(browser);
-
-    }
-
-    private Node createSpacer() {
-        Region spacer = new Region();
-        HBox.setHgrow(spacer, Priority.ALWAYS);
-        return spacer;
-    }
-
-    @Override
-    protected void layoutChildren() {
-        double w = getWidth();
-        double h = getHeight();
-        layoutInArea(browser, 0, 0, w, h, 0, HPos.CENTER, VPos.CENTER);
-    }
-
-    @Override
-    protected double computePrefWidth(double height) {
-        return 750;
-    }
-
-    @Override
-    protected double computePrefHeight(double width) {
-        return 500;
     }
 }
